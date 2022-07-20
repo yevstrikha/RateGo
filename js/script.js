dd04055a6f1e0da748df6033734c87cd63d75063
yevstrikha <yevstrikha@gmail.om>
2022-07-20 09:08:37 -0400

    add inner

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var trigger = document.getElementById('toggle');
+var box = document.getElementById('menu');
+
+toggle.addEventListener('click', function() {
+    box.classList.toggle('active');
+});
